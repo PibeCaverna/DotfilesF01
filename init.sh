2b9a78757257a41a2d4fb9eeac37e631a0c2c714
Franco Aniello Falconieri <francoafalconieri@gmail.com>
2025-02-24 21:55:51 -0300

    fixed ly install, should check why the xinitrc option launches poorly

--- init.sh
+++ init.sh
@@ -79,8 +79,7 @@ apt install libpam0g-dev libxcb-xkb-dev
 git clone https://github.com/fairyglade/ly
 cd ly 
 wget https://ziglang.org/download/0.12.0/zig-linux-x86_64-0.12.0.tar.xz
-tar xf https://ziglang.org/download/0.12.0/zig-linux-x86_64-0.12.0.tar.xz 
-./zig-linux-x86_64-0.12.0/zig build
+tar -xf https://ziglang.org/download/0.12.0/zig-linux-x86_64-0.12.0.tar.xz 
 ./zig-linux-x86_64-0.12.0/zig build installsystemd
 systemctl enable ly.service
 
